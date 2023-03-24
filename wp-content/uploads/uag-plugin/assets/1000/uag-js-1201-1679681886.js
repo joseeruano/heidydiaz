1a21d3aaa82ebfd992c0cbf3bd61dd22c1e96bd2
José Eduardo Ruano <joseeduardoruano@gmail.com>
2023-03-24 12:19:39 -0600

    nuevas descripciones y botones

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1201-1679681886.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-35b9f9c6 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-35b9f9c6 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-35b9f9c6 .swiper-button-next","prevEl":".uagb-block-35b9f9c6 .swiper-button-prev"}}	);
+});
+
+ })
