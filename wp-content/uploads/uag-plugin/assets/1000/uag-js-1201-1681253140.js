10e5c5034ac5d8661d9d52a66c0d2b5d3ea141b2
José Eduardo Ruano <joseeduardoruano@gmail.com>
2023-04-11 16:48:03 -0600

    menu on mobile

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1201-1681253140.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-cfff4236 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-cfff4236 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-cfff4236 .swiper-button-next","prevEl":".uagb-block-cfff4236 .swiper-button-prev"}}	);
+});
+
+ });
