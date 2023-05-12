caaceaa99b8c05e447837ac2216e234485bb2de3
José Eduardo Ruano <joseeduardoruano@gmail.com>
2023-05-12 15:18:32 -0600

    cambios en copy, logos y diseño general

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1201-1683923754.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-19977e5e .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-19977e5e .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-19977e5e .swiper-button-next","prevEl":".uagb-block-19977e5e .swiper-button-prev"}}	);
+});
+
+ });
